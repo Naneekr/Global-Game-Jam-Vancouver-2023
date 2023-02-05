9696f53fb0fd3160d873d3ed4cc633eefa767016
Keenan Render <keenanrender@gmail.com>
2023-02-05 11:15:46 -0800

    enemy spawner

---
+++ GGJV23_Roots/Enemy Spawner.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+var skele = preload("res://Scenes/skele.tscn")
+var bat = preload("res://Scenes/bat.tscn")
+
+var spawned_enemy
+
+@onready var spawn_pointL = $Spawn_Point_L
+@onready var spawn_pointR = $Spawn_Point_R
+
+var wave_timer = 5
+var wave_time_elapsed = 0
+var total_time_elapsed = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	total_time_elapsed += delta
+	wave_time_elapsed += delta
+	if wave_time_elapsed >= wave_timer:
+		spawn()
+	print(wave_time_elapsed)
+		
+
+func spawn():
+	
+	wave_time_elapsed = 0
+	var coin = randi_range(0,1)
+	var coin2 = randi_range(0,1)
+	
+	if coin == 0 : 
+		spawned_enemy = skele.instantiate()
+		add_child(spawned_enemy)
+		if coin2 == 0:
+			spawned_enemy.set_position(spawn_pointL.position)
+		if coin2 == 1:
+			spawned_enemy.set_position(spawn_pointR.position)
+			
+	if coin == 1 :
+		spawned_enemy = bat.instantiate()
+		add_child(spawned_enemy)
+		if coin2 == 0:
+			spawned_enemy.set_position(spawn_pointL.position)
+		if coin2 == 1:
+			spawned_enemy.set_position(spawn_pointR.position)
+	
